223febd740a82c0a22ecdadef0163a0e43f39d3c
Erkka Heinilä <erkkahe@gmail.com>
2023-11-05 20:46:59 +0200

    Fix vellumo channel

--- configuration.nix
+++ configuration.nix
@@ -264,7 +264,7 @@ in
         image = "ghcr.io/teekuningas/vellubot/vellubot:${vellubotVersion}";
         autoStart = true;
         environment = {
-          BOT_CHANNEL = "#vellumotest";
+          BOT_CHANNEL = "#vellumo";
           BOT_NICKNAME = "vellubot";
           BOT_SERVER = "irc.libera.chat";
           BOT_PORT = "6667";
