7530a6b56ba45c032ebeef1a97d55eb0f4909ed6
Erkka Heinilä <erkkahe@gmail.com>
2023-11-11 18:32:42 +0200

    Bump vellubot

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@ let
   imagellmApiVersion = "v8";
   soitbeginsFrontendVersion = "0.1.0";
   soitbeginsBackendVersion = "0.1.0";
-  vellubotVersion = "0.14.0";
+  vellubotVersion = "0.15.0";
   secretsFile = "/etc/nixos/secrets.nix";
   secrets =
     if builtins.pathExists secretsFile
