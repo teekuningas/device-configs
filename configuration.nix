f82af71c95096eabcc026f955ce66fa0603ecb56
Erkka Heinilä <erkkahe@gmail.com>
2024-10-22 16:07:55 +0300

    Update lobechat

--- configuration.nix
+++ configuration.nix
@@ -217,7 +217,7 @@ in
         autoStart = true;
       };
       lobechat = {
-        image = "docker.io/lobehub/lobe-chat:v1.6.0";
+        image = "docker.io/lobehub/lobe-chat:v1.22.17";
         ports = ["127.0.0.1:3210:3210"];
         autoStart = true;
       };
