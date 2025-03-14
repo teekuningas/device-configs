98ba9eaa5116af57d2a58af02675a645e0284d29
Erkka Heinilä <erkkahe@gmail.com>
2025-03-14 15:03:25 +0200

    procyon: update env variables

--- procyon/configuration.nix
+++ procyon/configuration.nix
@@ -133,7 +133,7 @@
     OLLAMA_HOST = "https://jyu2401-62.tail5b278e.ts.net/ollamapi";
     # aider
     OLLAMA_API_BASE = "https://jyu2401-62.tail5b278e.ts.net/ollamapi";
-    AIDER_MODEL = "gemma3:27b";
+    AIDER_MODEL = "ollama_chat/gemma3:27b";
     AIDER_MAP_TOKENS = "8192";
     AIDER_MAX_CHAT_HISTORY_TOKENS="32768";
   };
