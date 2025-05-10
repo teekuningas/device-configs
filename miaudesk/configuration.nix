4a2fcef41c6f3fb8f9c1ef60e3c4829afa8c54a5
teekuningas <erkkahe@gmail.com>
2025-05-10 18:01:08 +0000

    miaudesk: update

--- miaudesk/configuration.nix
+++ miaudesk/configuration.nix
@@ -44,6 +44,7 @@
       folium
       twine
       (ps.callPackage "${inputs.teepkgs}/pkgs/llm-azure/default.nix" {python3Packages = ps;})
+      (ps.callPackage "${inputs.teepkgs}/pkgs/llm-echo/default.nix" {python3Packages = ps;})
       (ps.callPackage "${inputs.teepkgs}/pkgs/ospeak/default.nix" {python3Packages = ps;})
       (ps.callPackage "${inputs.teepkgs}/pkgs/files-to-prompt/default.nix" {python3Packages = ps;})
     ]))
