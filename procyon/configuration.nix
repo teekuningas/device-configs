db0193775c49d0178f8fec6d6a00ad70e5492c95
Erkka Heinilä <erkkahe@gmail.com>
2026-04-30 09:07:49 +0300

    Update procyon safepilot config

--- procyon/configuration.nix
+++ procyon/configuration.nix
@@ -235,9 +235,10 @@ in
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   programs.safepilot = {
     enable = true;
-    defaultArgs = [ "--git" "--copilot" "--gemini" "--npm" ];
+    defaultArgs = [ "--git" "--copilot" "--gemini" "--opencode" "--npm" ];
     withCopilot = true;
     withGemini = true;
+    withOpencode = true;
   };
 
   system.stateVersion = "24.05"; # Did you read the comment?
